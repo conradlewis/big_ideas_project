2180c11304a22916d4e2506919366264240eca02
Conrad Lewis <conrad@Conrads-MBP.68lookout.local>
2020-04-11 18:03:25 -0400

    move method

--- game/fish.js
+++ game/fish.js
@@ -1,63 +1,20 @@
-var movingRight = true;
-var movingLeft = false;
-function move(goRight, goLeft)
-{
-    if(movingRight)
-    {
-        moveRight(goRight);
-    }
-    if(movingLeft)
-    {
-        moveLeft(goLeft);
-    }
-
-}
-function moveRight(startPos) 
+function move(startPos, endPos, direction)
 {
     var pos = startPos;
     var element = document.getElementById("fish");
-    element.style.top = pos + 'px';
+    element.style.top = '50px';
     
     var id = setInterval(frame, 5);
     
     function frame() 
     {
-        if (pos == 880) 
-        {
+        if (pos == endPos) 
             clearInterval(id);
-            movingRight = false;
-            movingLeft = true;
-        }
         
         else 
         {
-            pos++;
+            pos += direction;
             element.style.left = pos + 'px';
-        }
-    }
-}
-function moveLeft(startPos2) 
-{
-    var pos2 = startPos2;
-    var element = document.getElementById("fish");
-    element.style.top = pos2 + 'px';
-    element.style.right = pos2 + 'px';
-    
-    var id = setInterval(frame, 5);
-    
-    function frame() 
-    {
-        if (pos2 == 880) 
-        {
-            clearInterval(id);
-            movingLeft = false;
-            movingRight = true;
-        }
-        
-        else 
-        {
-            pos2++;
-            element.style.right = pos2 + 'px';
         }
     }
 }
