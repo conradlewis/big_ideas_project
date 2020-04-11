c89643660785974a460d8329d19ba9ece2f0d097
Shuangping Shi <ShuShi@PROGENICS.COM>
2020-04-11 17:02:13 -0400

    added blub blub backward

--- game/fish.js
+++ game/fish.js
@@ -1,3 +1,17 @@
+var movingRight = true;
+var movingLeft = false;
+function move(goRight, goLeft)
+{
+    if(movingRight)
+    {
+        moveRight(goRight);
+    }
+    if(movingLeft)
+    {
+        moveLeft(goLeft);
+    }
+
+}
 function moveRight(startPos) 
 {
     var pos = startPos;
@@ -11,12 +25,39 @@ function moveRight(startPos)
         if (pos == 880) 
         {
             clearInterval(id);
+            movingRight = false;
+            movingLeft = true;
         }
         
         else 
         {
             pos++;
             element.style.left = pos + 'px';
+        }
+    }
+}
+function moveLeft(startPos2) 
+{
+    var pos2 = startPos2;
+    var element = document.getElementById("fish");
+    element.style.top = pos2 + 'px';
+    element.style.right = pos2 + 'px';
+    
+    var id = setInterval(frame, 5);
+    
+    function frame() 
+    {
+        if (pos2 == 880) 
+        {
+            clearInterval(id);
+            movingLeft = false;
+            movingRight = true;
+        }
+        
+        else 
+        {
+            pos2++;
+            element.style.right = pos2 + 'px';
         }
     }
 }
