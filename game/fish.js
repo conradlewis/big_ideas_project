8ba50ce3f1a2c047798bbc9a6e9d8e39ee9ba9d7
Conrad Lewis <conrad@Conrads-MBP.68lookout.local>
2020-04-10 22:00:19 -0400

    added fish + background

--- game/fish.js
+++ game/fish.js
@@ -1,9 +1,19 @@
-function drawFish()
-{
-
-}
-
 function swim()
 {
-
+    var element = document.getElementById("fish1"); 
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() 
+    {
+        if (pos == 350) 
+        {
+            clearInterval(id);
+        } 
+        
+        else 
+        {
+            element.style.left = pos + 'px';
+            element.style.left = pos + 'px'; 
+        }
+    }
 }
