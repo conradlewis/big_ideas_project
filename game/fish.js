c478e07af58ccf5305480777189c4fdb50dba2f5
conrad <conrad@conrads-mbp.68lookout.local>
2020-04-10 22:29:58 -0400

    I hate Eclipse for HTML

--- game/fish.js
+++ game/fish.js
@@ -1,18 +1,22 @@
-function swim(initPos)
+function moveRight() 
 {
-    var element = document.getElementById("fish1"); 
+    var element = document.getElementById("fish1");   
+    var pos = 30;
+    element.style.top = pos + 'px';
+
     var id = setInterval(frame, 5);
+    
     function frame() 
     {
-        if (pos == 350) 
+        if (pos == 880)
         {
             clearInterval(id);
-        } 
-        
+        }
+ 
         else 
         {
-            element.style.left = initPos + 'px';
-            element.style.left = initPos + 'px'; 
+            pos++; 
+            element.style.left = pos + 'px'; 
         }
     }
 }
