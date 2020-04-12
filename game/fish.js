163423c8d619c16145da58f852437fb7a33f0ac6
Shuangping Shi <ShuShi@PROGENICS.COM>
2020-04-11 20:22:25 -0400

    they've evolved minds of their own

--- game/fish.js
+++ game/fish.js
@@ -1,20 +1,46 @@
-function move(startPos, endPos, direction)
+// function move(startPos, endPos, direction)
+// {
+//     var pos = startPos;
+//     var element = document.getElementById("fish");
+//     element.style.top = '50px';
+    
+//     var id = setInterval(frame, 5);
+    
+//     function frame() 
+//     {
+//         if (pos == endPos) 
+//             clearInterval(id);
+        
+//         else 
+//         {
+//             pos += direction;
+//             element.style.left = pos + 'px';
+//         }
+//     }
+// }
+function move(startPos, endPos)
 {
+    var direction = 1;
     var pos = startPos;
     var element = document.getElementById("fish");
     element.style.top = '50px';
+    element.style.left = '30px';
     
     var id = setInterval(frame, 5);
     
     function frame() 
     {
+
         if (pos == endPos) 
-            clearInterval(id);
-        
-        else 
         {
-            pos += direction;
-            element.style.left = pos + 'px';
+            direction *= -1;
+            var temp = startPos;
+            startPos = endPos;
+            endPos = temp;
+            // clearInterval(id);
         }
+        pos += direction;
+        element.style.left = pos + 'px';
+
     }
 }
