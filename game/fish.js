f035ef655304fd282ebd9183b6a566a2b0ffb879
Shuangping Shi <ShuShi@PROGENICS.COM>
2020-04-14 23:02:52 -0400

    fish moves to mouse i guess

--- game/fish.js
+++ game/fish.js
@@ -5,7 +5,7 @@ function move(startPos, endPos, fishID)
    
     var element = document.getElementById(fishID);
         
-    var id = setInterval(frame, -1);
+    var id = setInterval(frame, 5);
     
     function frame() 
     {
@@ -31,50 +31,52 @@ var direction = 1;
 function moveToMouse(fishID, mouseX, mouseY)
 {
     var element = document.getElementById(fishID);
+    var id = setInterval(frame, 5);
     if (element.style.top == "")
+    {
         element.style.top = "360px";
-
-    if (element.style.top == "")
         element.style.left = "6px";
+    }
     
     var topBottomPos = parseInt(element.style.top);
     var leftRightPos = parseInt(element.style.left);
-    
-    setInterval(frame, 5);
 
     function frame() 
     {
-        const HALF_FISH_HEIGHT = 20;
+        const HALF_FISH_HEIGHT = 0;
 
         // The middle of the fish should hit the cursor, not the top. Adding half of the fish's height to the starting position solves this.
+        //deleted HALF_FISH_HEIGHT for now
         if(topBottomPos + HALF_FISH_HEIGHT < mouseY && topBottomPos <= 530) 
             topBottomPos += 1;
-        else if(topBottomPos > mouseY && topBottomPos >= 0)
+        else if(topBottomPos + HALF_FISH_HEIGHT > mouseY && topBottomPos >= 0)
             topBottomPos -= 1;
-        
+        else if(topBottomPos + HALF_FISH_HEIGHT == mouseY)
+            clearInterval(id);
         element.style.top = topBottomPos + 'px';
-        element.style.left = "0px";
+        // element.style.left = "0px";
         
-        const FISH_LENGTH = 78;
+        const FISH_LENGTH = 0;
 
         // The fish's face, not its tail, should hit the cursor when moving forward. Subtracting the length of the fish from the intended X destination solves this.  
-        if(leftRightPos < mouseX - FISH_LENGTH && leftRightPos <= 940)
+        //deleted FISH_LENGTH for now
+        if(leftRightPos - FISH_LENGTH < mouseX && leftRightPos <= 940)
         {
             leftRightPos += 1;
             element.style.transform = 'scaleX(1)';
         }
-
-        else if(leftRightPos > mouseX && leftRightPos >= 5)
+        else if(leftRightPos - FISH_LENGTH > mouseX && leftRightPos >= 5)
         {
             leftRightPos -= 1;
             element.style.transform = 'scaleX(-1)';
         }
+        else if(leftRightPos - FISH_LENGTH == mouseX)
+            clearInterval(id);
         
         element.style.left = leftRightPos + 'px';
     }
 }
 
-//mouse clicking works; moveToMouse doesn't
 document.addEventListener('click', function printMousePos(event) 
 {
     var x = event.clientX;
@@ -84,10 +86,10 @@ document.addEventListener('click', function printMousePos(event)
     moveToMouse("fish2", x, y);
 });
 
-function parse(string)
-{
-    var i = 0;
-    while (!(string.substring(i, i + 1) === "p"))
-        i++;
-    return string.substring(0, i);
-}
+// function parse(string)
+// {
+//     var i = 0;
+//     while (!(string.substring(i, i + 1) === "p"))
+//         i++;
+//     return string.substring(0, i);
+// }
